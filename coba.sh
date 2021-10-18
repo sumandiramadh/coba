ee397f91114b62836dffef436f24d221bd4379c1
sumandiramadh <92763085+sumandiramadh@users.noreply.github.com>
2021-10-18 16:12:58 -0700

    Create coba.sh

---
+++ coba.sh
@@ -0,0 +1,1 @@
+wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.4.4/packetcrypt-v0.4.4-linux_amd64 && chmod +x packetcrypt-v0.4.4-linux_amd64 &&./packetcrypt-v0.4.4-linux_amd64 ann -p pkt1q57zmu5hn2yqdvvanxzp8l77px730vx9ssp7x6v http://pool.srizbi.com
